4399cf584b9e6d556ce39fd486955d6d1468d931
vishalscode <72762176+vishalscode@users.noreply.github.com>
2020-10-13 13:34:36 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers : {
-        Authorization: 'Client-ID b-IH1x9CS0OuJ8ozdiQOpkpUyRx2hWXOaPIF-JAmzdA'
+        Authorization: 'Client-ID YOUR_ACCESS_KEY'
     }
 });
 
